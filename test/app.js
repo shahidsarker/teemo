6d97f4e74e0a14ad861cff68bb159eee17a973a9
Shahid <shahid.sarker@gmail.com>
2020-03-04 17:12:05 -0500

    remove twilio

--- test/app.js
+++ test/app.js
@@ -41,14 +41,6 @@ describe("GET /contact", () => {
   });
 });
 
-describe("GET /api/twilio", () => {
-  it("should return 200 OK", done => {
-    request(app)
-      .get("/api/twilio")
-      .expect(200, done);
-  });
-});
-
 describe("GET /api/scraping", () => {
   it("should return 200 OK", done => {
     request(app)
